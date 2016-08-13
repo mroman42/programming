bd98eaefac3e502b7d000a20081025ca4d9272f8
M42 <mromang08@gmail.com>
2016-08-13 15:48:06 +0200

    Fibonacci

---
+++ fibonacci.hs
@@ -0,0 +1,15 @@
+import Control.Monad
+
+modulo = 100000000 + 7
+(+.) = \x -> \y -> mod (x+y) modulo
+fibonacci = [0,1] ++ (zipWith (+.) fibonacci (tail fibonacci))
+fib = (fibonacci !!)
+
+main = do
+  cases <- readLn
+  replicateM_ cases (do
+                        n <- readLn
+                        print $ fib n
+                    )
+                        
+  
